b59b68f8a0d322441b832e9730bf62d775bd4bbf
adrianomonteirodev <adrianomonteiroweb@gmail.com>
2022-02-18 11:26:25 -0300

    feat: initial states and onchange function in input name.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
+let initialState = {
+  smartphone: 0,
+  smarttv: 0,
+  notebook: 0,
+  lastBidOnSmartphone: '',
+  lastBidOnSmarttv: '',
+  lastBidOnNotebook: '',
+};
+
 export default function App() {
+  const {
+    smartphone,
+    smarttv,
+    notebook,
+  } = initialState;
+
+  // initial state of items
+  const [phone, setPhone] = useState(smartphone);
+  const [tv, setTv] = useState(smarttv);
+  const [note, setNote] = useState(notebook);
+
+  // initial state of last bid.
+  const [lastBidOnSmartphone,setLastBidOnSmartphone] = useState('');
+  const [lastBidOnSmarttv,setLastBidOnSmarttv] = useState('');
+  const [lastBidOnNotebook,setLastBidOnNotebook] = useState('');
+
+  // initial state of buttons
+  const [smartphoneButton, setSmartphoneButton] = useState('Dar lance');
+  const [smarttvButton, setSmarttvButton] = useState('Dar lance');
+  const [notebookButton, setNotebookButton] = useState('Dar lance');
+
+  const [smartphoneDisabled, setSmartphoneDisabled] = useState(false);
+  const [smarttvDisabled, setSmarttvDisabled] = useState(false);
+  const [notebookDisabled, setNotebookDisabled] = useState(false);
+
+  // initial state of name
+  const [name, setName] = useState();
+
+  function handleChange({ target: { value } }) {
+    setName(value);
+  }
+
   return (
     <div className="App">
       <h1>Leilão de Centavos</h1>
-      Nome: <input type="text" name="name" />
+      Nome: <input type="text" name="name" onChange={ handleChange } />
       <p></p>
       <div className="flex-container">
         <div>
